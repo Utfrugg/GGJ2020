da8257fab4183e01f9492d625d5061b5624bcb23
JoaquinAguilera <jndragneel@gmail.com>
2020-01-31 23:58:46 +0100

    Added player targeting interactables
    + Debug message in interactable

--- RadDragonsGGJ/Assets/Scripts/Interactable.cs
+++ RadDragonsGGJ/Assets/Scripts/Interactable.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnInteract()
+    {
+        Debug.Log("Hey I got interacted with");
     }
 }
