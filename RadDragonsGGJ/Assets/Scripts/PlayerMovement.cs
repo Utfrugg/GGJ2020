54bc6c5b6917d5e35aef6e7ee2af3eb7e3db5e63
Utfrugg <170462@ade-nhtv.nl>
2020-01-31 22:40:26 +0100

    Basic Movement

--- RadDragonsGGJ/Assets/Scripts/PlayerMovement.cs
+++ RadDragonsGGJ/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D body;
+    private float horizontal = 0.0f;
+    private float vertical = 0.0f;
+
+    public float runSpeed = 20.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    { 
+
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
 }
