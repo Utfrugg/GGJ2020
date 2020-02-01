445059f8ad29c5e2606343443a7bec32f86900c7
Utfrugg <170462@ade-nhtv.nl>
2020-02-01 20:44:13 +0100

    Holes n oxygen and stuff

---
+++ RadDragonsGGJ/Assets/Scripts/Hole.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var controller = other.attachedRigidbody.gameObject.GetComponent<PlayerController>();
+        if (controller != null)
+        {
+            controller.losingOxygen = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        var controller = other.attachedRigidbody.gameObject.GetComponent<PlayerController>();
+        if (controller != null)
+        {
+            controller.losingOxygen = false;
+        }
+    }
+}
