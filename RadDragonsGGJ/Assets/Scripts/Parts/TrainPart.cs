2319b1a02f5b11604865e703b52258f49114d19a
Utfrugg <170462@ade-nhtv.nl>
2020-02-01 10:48:19 +0100

    Static interactable

---
+++ RadDragonsGGJ/Assets/Scripts/Parts/TrainPart.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum PartState
+{
+    GOOD,
+    OVERHEATED,
+    BURNING,
+    BROKEN
+}
+
+public class TrainPart : Interactable
+{
+    private PartState currentState = PartState.GOOD;
+
+    public float heat = 0.0f;
+    public float health = 100.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case PartState.GOOD:
+                break;
+            case PartState.OVERHEATED:
+                break;
+            case PartState.BURNING:
+                break;
+            case PartState.BROKEN:
+                break;
+        }
+    }
+
+    public override void OnInteract()
+    {
+        Debug.Log("PartBoi");
+    }
+}
