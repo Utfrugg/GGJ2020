4e71e3060c00e941a55bb358009922c8ff0edfb5
Utfrugg <170462@ade-nhtv.nl>
2020-02-01 16:39:52 +0100

    Phase processing

--- RadDragonsGGJ/Assets/Scripts/Train.cs
+++ RadDragonsGGJ/Assets/Scripts/Train.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Train : MonoBehaviour
 {
+    struct Phase
+    {
+        public uint numPartsBreaking;
+        //public uint numEvents; //TODO implement events first
+        public float duration;
+
+        public Phase(uint parts, float dur)
+        {
+            numPartsBreaking = parts;
+            duration = dur;
+        }
+    }
+
+
     private static Train instance;
 
     public static Train Instance
@@ -16,6 +31,10 @@ public class Train : MonoBehaviour
     public List<PlayerController> players;
     public List<Interactable> interactables;
     public List<TrainPart> trainParts;
+
+    private Queue<Phase> phases = new Queue<Phase>();
+    private IEnumerator processPhase;
+    private int phaseProgress = 0;
 
     private int numPlayers = 1;
     private bool frozen = false;
@@ -36,6 +55,14 @@ public class Train : MonoBehaviour
     void Start()
     {
         numPlayers = players.Count;
+
+        phases.Enqueue(new Phase(1, 3));
+        phases.Enqueue(new Phase(1, 4));
+        phases.Enqueue(new Phase(1, 5));
+        phases.Enqueue(new Phase(1, 6));
+
+
+        InitiatePhases();
     }
 
     // Update is called once per frame
@@ -74,5 +101,58 @@ public class Train : MonoBehaviour
         }
 
         frozen = false;
+    }
+
+    private void InitiatePhases()
+    {
+        if (phases.Count == 0)
+        {
+            Debug.LogError("There are no phases in the queue on start up");
+        }
+
+        ProcessPhase(phases.Dequeue());
+    }
+
+    private IEnumerator WaitForNextPhase(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+
+        if (phases.Count != 0)
+        {
+            ProcessPhase(phases.Dequeue());
+        }
+    }
+
+    private void ProcessPhase(Phase phase)
+    {
+        Debug.Log($"Processing phase. Duration: {phase.duration}");
+        for (int i = 0; i < phase.numPartsBreaking; ++i)
+        {
+            int randomIndex = Random.Range(0, trainParts.Count - 1);
+
+            DamagePart(trainParts[randomIndex]);
+
+        }
+
+        StartCoroutine(WaitForNextPhase(phase.duration));
+    }
+
+    private void DamagePart(TrainPart trainPart)
+    {
+        switch (trainPart.currentState)
+        {
+            case PartState.GOOD:
+                trainPart.currentState = PartState.WARMINGUP;
+                break;
+            case PartState.WARMINGUP:
+                trainPart.currentState = PartState.BURNING;
+                break;
+            case PartState.BURNING:
+                trainPart.currentState = PartState.BROKEN;
+                break;
+            default:
+                Debug.Log("Part is broken already");
+                break;
+        }
     }
 }
