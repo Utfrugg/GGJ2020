6525b782c3b3a319dfd61017d864fb100bd24958
JoaquinAguilera <jndragneel@gmail.com>
2020-02-01 06:44:46 +0100

    Made Targeting dependent on rotation

--- RadDragonsGGJ/Assets/Scripts/PlayerController.cs
+++ RadDragonsGGJ/Assets/Scripts/PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GetComponent<PlayerInteraction>().UpdateTarget(transform.position, new Vector2(0, 1));
+        GetComponent<PlayerInteraction>().UpdateTarget(transform.position);
 
         if (Input.GetKeyDown("joystick " + PlayerNumber + " button 0"))
         {
