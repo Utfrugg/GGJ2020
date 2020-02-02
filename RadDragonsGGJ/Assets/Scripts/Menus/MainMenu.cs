8ddbc1fcb136ffd3c84b6d9c8e0fc883bf56c853
Utfrugg <170462@ade-nhtv.nl>
2020-02-02 02:08:39 +0100

    Main Menu

---
+++ RadDragonsGGJ/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var button = transform.Find("Start");
+        if (button != null)
+        {
+            EventSystem.current.SetSelectedGameObject(button.gameObject);
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
