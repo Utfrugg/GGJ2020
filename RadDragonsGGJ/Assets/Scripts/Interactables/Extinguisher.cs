06c19cbf58215b72b895e344d236ae12cf04b516
roygamedevelopment <roygamedev@gmail.com>
2020-02-02 01:06:41 +0100

    [Add] Smoke particles [Change] Updated level colour lerp thing

--- RadDragonsGGJ/Assets/Scripts/Interactables/Extinguisher.cs
+++ RadDragonsGGJ/Assets/Scripts/Interactables/Extinguisher.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Extinguisher : Interactable
 {
+
+    public GameObject splashPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +22,8 @@ public class Extinguisher : Interactable
     public override void OnInteract(Interactable itemUsed, bool holding)
     {
         Debug.Log("DIE FLAME!");
+        GameObject Splash = Instantiate(splashPrefab, transform.position + transform.up * 1.0f, transform.rotation);
+        Splash.GetComponent<Rigidbody2D>().velocity = Quaternion.AngleAxis(Random.value * 60 - 30, transform.forward) * transform.up * 10;
+        //splashPrefab.GetComponent<Rigidbody2D>().AddForce(new Vector2(this.transform.forward.x, this.transform.forward.z) * 3.0f);
     }
 }
